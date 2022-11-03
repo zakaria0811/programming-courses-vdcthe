1d7902c5675f0e2401f300c637450e9c26391fad
zakaria0811 <116682735+zakaria0811@users.noreply.github.com>
2022-11-03 20:14:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-vdcthe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-vdcthe)
---
+++ index.ts
@@ -0,0 +1,10 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let zakii: number;
+zakii = parseInt (prompt("right a number inferieur a 100"));
+while (zakii<=100){
+  zakii = parseInt(prompt("please right a number <= 100));
+}
+console.log(zakii);
